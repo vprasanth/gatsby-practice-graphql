1588916c2a56089506a3426755fda19c53267f43
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2018-03-03 22:37:25 -0500

    Use local filesystem plugin to get data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  plugins: [{
+  plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`
+    }
+  }, 
+  {
     resolve: `gatsby-plugin-typography`,
     options: {
       pathToConfigModule: `src/utils/typography.js`
